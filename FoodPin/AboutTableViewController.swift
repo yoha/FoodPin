380a608755b9f5bd16a5d569d663d14a17589027
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2016-01-10 19:20:46 +0700

    23. "Creating a Custom Class for the About View Controller"

---
+++ FoodPin/AboutTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  AboutTableViewController.swift
+//  FoodPin
+//
+//  Created by Yohannes Wijaya on 1/10/16.
+//  Copyright © 2016 Yohannes Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class AboutTableViewController: UITableViewController {
+    
+    // MARK: - Stored Properties
+    
+    let sectionTitles = ["Leave Feedback", "Follow US"]
+    let sectionContent = [["Rate us on App Store", "Tell us your feedback"], ["Twitter", "Facebook", "Pinterest"]]
+    let links = ["https://twitter.com/appcodamobile", "https://facebook.com/appcodamobile", "https://pinterest.com/appcoda/"]
+
+    // MARK: - UITableViewDataSource Methods
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let firstSectionRowCount = self.sectionContent.first?.count else { return 0 }
+        guard let secondSectionRowCount = self.sectionContent.last?.count else { return 0 }
+        return section == 0 ? firstSectionRowCount : secondSectionRowCount
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("AboutTableViewCell", forIndexPath: indexPath)
+        
+        guard let validTextLabel: String = self.sectionContent[indexPath.section][indexPath.row] else { return cell }
+        cell.textLabel?.text = validTextLabel
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.sectionTitles[section]
+    }
+
+}
